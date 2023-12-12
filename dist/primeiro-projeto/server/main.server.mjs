0dfa97cee187309c2c2836a4c34b400191d0a56c
joebercoutinho <125513833+JoeberCoutinho@users.noreply.github.com>
2023-12-12 16:03:38 -0300

    primeiro commit

---
+++ dist/primeiro-projeto/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-QIG35XXO.mjs";import"./chunk-F5GZXTTI.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
---
+++ src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+import { FirstComponentComponent } from './componentes/first-component/first-component.component';
+export const routes: Routes = [
+    {path:'',component: FirstComponentComponent},
+];
